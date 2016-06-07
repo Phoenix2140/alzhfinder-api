af22a8ec80e4abb07a39a79af76d644ef8d69a4d
Héctor Álvarez <phoenix@altro>
2016-06-07 18:43:04 -0400

    vista básica

--- v1/app/controllers/Home.php
+++ v1/app/controllers/Home.php
@@ -36,9 +36,9 @@
 			 * los valores de la tabla y se asignan a las variables de
 			 * la vista
 			 */
-			require($this->config->get('modelsDir').'Tabla.php');
-			$tabla = new Tabla($this->config);
-			$this->view->datos = $tabla->getTabla();
+			// require($this->config->get('modelsDir').'Tabla.php');
+			// $tabla = new Tabla($this->config);
+			// $this->view->datos = $tabla->getTabla();
 			
 			/**
 			 * Se crean variables (titulo y mensaje) en la vista.
@@ -50,7 +50,7 @@
 			 * Se crea una variable (especial) que contiene una vista
 			 * views/home.php, con los valores deseados
 			 */
-			$this->view->content = $this->view->render($this->config->get('viewsDir').'home.php');
+			// $this->view->content = $this->view->render($this->config->get('viewsDir').'home.php');
 
 			/**
 			 * Luego se genera y junta toda la vista en 
@@ -58,8 +58,8 @@
 			 * en su interior todas las variables creadas anteriormente
 			 * incluso la vista parcial home.php
 			 */
-			echo $this->view->render($this->config->get('viewsDir').'header.php');
-
+			// echo $this->view->render($this->config->get('viewsDir').'header.php');
+			echo "versión alpha en construcción".
 
 		}
 	}
